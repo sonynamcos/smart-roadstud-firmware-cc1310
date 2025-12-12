5bc3f82d84462df3d17a8e60891914e6bb56b33b
Jun Kim <sonynamco@gmail.com>
2025-12-12 10:13:59 +0900

    Initial firmware project: based on rfEasyLinkRx

---
+++ rfPacketRx.c
@@ -0,0 +1,181 @@
+/*
+ * Copyright (c) 2019, Texas Instruments Incorporated
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * *  Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * *  Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * *  Neither the name of Texas Instruments Incorporated nor the names of
+ *    its contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/***** Includes *****/
+/* Standard C Libraries */
+#include <stdlib.h>
+#include <string.h>
+
+/* TI Drivers */
+#include <ti/drivers/rf/RF.h>
+#include <ti/drivers/PIN.h>
+#include <ti/sysbios/knl/Task.h>
+#include <ti/sysbios/knl/Clock.h>
+
+/* Driverlib Header files */
+#include DeviceFamily_constructPath(driverlib/rf_prop_mailbox.h)
+
+/* Board Header files */
+#include "Board.h"
+#include "CC1310_LAUNCHXL.h"   // ← 이 파일에서 NODE_SK6812_DIN을 가져옴
+
+/* Application Header files */
+#include "RFQueue.h"
+#include "smartrf_settings/smartrf_settings.h"
+
+/***** Defines *****/
+
+/* Packet RX Configuration */
+#define DATA_ENTRY_HEADER_SIZE 8  /* Constant header size of a Generic Data Entry */
+#define MAX_LENGTH             30 /* Max length byte the radio will accept */
+#define NUM_DATA_ENTRIES       2  /* NOTE: Only two data entries supported at the moment */
+#define NUM_APPENDED_BYTES     2  /* The Data Entries data field will contain:
+                                   * 1 Header byte (RF_cmdPropRx.rxConf.bIncludeHdr = 0x1)
+                                   * Max 30 payload bytes
+                                   * 1 status byte (RF_cmdPropRx.rxConf.bAppendStatus = 0x1) */
+
+
+
+/***** Prototypes *****/
+static void callback(RF_Handle h, RF_CmdHandle ch, RF_EventMask e);
+
+/***** Variable declarations *****/
+static RF_Object rfObject;
+static RF_Handle rfHandle;
+
+/* Pin driver handle */
+static PIN_Handle ledPinHandle;
+static PIN_State ledPinState;
+
+static PIN_State  sk6812PinState;
+static PIN_Handle sk6812PinHandle;
+
+static PIN_Config sk6812PinTable[] = {
+    NODE_SK6812_DIN | PIN_GPIO_OUTPUT_EN | PIN_GPIO_LOW | PIN_PUSHPULL | PIN_DRVSTR_MAX,
+    PIN_TERMINATE
+};
+
+
+/* Buffer which contains all Data Entries for receiving data.
+ * Pragmas are needed to make sure this buffer is 4 byte aligned (requirement from the RF Core) */
+#if defined(__TI_COMPILER_VERSION__)
+#pragma DATA_ALIGN (rxDataEntryBuffer, 4);
+static uint8_t
+rxDataEntryBuffer[RF_QUEUE_DATA_ENTRY_BUFFER_SIZE(NUM_DATA_ENTRIES,
+                                                  MAX_LENGTH,
+                                                  NUM_APPENDED_BYTES)];
+#elif defined(__IAR_SYSTEMS_ICC__)
+#pragma data_alignment = 4
+static uint8_t
+rxDataEntryBuffer[RF_QUEUE_DATA_ENTRY_BUFFER_SIZE(NUM_DATA_ENTRIES,
+                                                  MAX_LENGTH,
+                                                  NUM_APPENDED_BYTES)];
+#elif defined(__GNUC__)
+static uint8_t
+rxDataEntryBuffer[RF_QUEUE_DATA_ENTRY_BUFFER_SIZE(NUM_DATA_ENTRIES,
+                                                  MAX_LENGTH,
+                                                  NUM_APPENDED_BYTES)]
+                                                  __attribute__((aligned(4)));
+#else
+#error This compiler is not supported.
+#endif
+
+/* Receive dataQueue for RF Core to fill in data */
+static dataQueue_t dataQueue;
+static rfc_dataEntryGeneral_t* currentDataEntry;
+static uint8_t packetLength;
+static uint8_t* packetDataPointer;
+
+
+static uint8_t packet[MAX_LENGTH + NUM_APPENDED_BYTES - 1]; /* The length byte is stored in a separate variable */
+
+/*
+ * Application LED pin configuration table:
+ *   - All LEDs board LEDs are off.
+ */
+PIN_Config pinTable[] =
+{
+    Board_PIN_LED2 | PIN_GPIO_OUTPUT_EN | PIN_GPIO_LOW | PIN_PUSHPULL | PIN_DRVSTR_MAX,
+	PIN_TERMINATE
+};
+
+/***** Function definitions *****/
+
+void *mainThread(void *arg0)
+{
+    RF_Params rfParams;
+    RF_Params_init(&rfParams);
+
+    /* Open LED pins */
+    ledPinHandle = PIN_open(&ledPinState, pinTable);
+    if (ledPinHandle == NULL)
+    {
+        while(1);
+    }
+
+    sk6812PinHandle = PIN_open(&sk6812PinState, sk6812PinTable);
+    if (sk6812PinHandle == NULL) while(1);
+    PIN_setOutputValue(sk6812PinHandle, NODE_SK6812_DIN, 1);
+    Task_sleep(10000 / Clock_tickPeriod);
+
+    while (1) {
+        PIN_setOutputValue(sk6812PinHandle, NODE_SK6812_DIN, 1);
+        Task_sleep(100000 / Clock_tickPeriod);
+        PIN_setOutputValue(sk6812PinHandle, NODE_SK6812_DIN, 0);
+        Task_sleep(100000 / Clock_tickPeriod);
+    }
+    
+}
+
+void callback(RF_Handle h, RF_CmdHandle ch, RF_EventMask e)
+{
+    if (e & RF_EventRxEntryDone)
+    {
+        /* Toggle pin to indicate RX */
+        PIN_setOutputValue(ledPinHandle, Board_PIN_LED2,
+                           !PIN_getOutputValue(Board_PIN_LED2));
+
+        /* Get current unhandled data entry */
+        currentDataEntry = RFQueue_getDataEntry();
+
+        /* Handle the packet data, located at &currentDataEntry->data:
+         * - Length is the first byte with the current configuration
+         * - Data starts from the second byte */
+        packetLength      = *(uint8_t*)(&currentDataEntry->data);
+        packetDataPointer = (uint8_t*)(&currentDataEntry->data + 1);
+
+        /* Copy the payload + the status byte to the packet variable */
+        memcpy(packet, packetDataPointer, (packetLength + 1));
+
+        RFQueue_nextEntry();
+    }
+}
